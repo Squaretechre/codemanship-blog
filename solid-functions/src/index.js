998e30832aeb82b823f46e803c7da86c7b34eef6
Dan Parkin <hello@danparkin.com>
2019-06-08 22:05:41 +0100

    feat: configure pricing strategy via query string

--- solid-functions/src/index.js
+++ solid-functions/src/index.js
@@ -2,10 +2,6 @@ import price from './pricer'
 import fetchRating from './imdb-service'
 import getScalar from './scalar-db'
 
-const buttonSubmit = document.querySelector(".price-calculator__submit")
-const fieldMovieId = document.querySelector(".price-calculator__movie-id")
-const labelPrice = document.querySelector(".price-calculator__price")
-
 const scalarDb = getScalar(
   'https://localhost:7979',
   'imdb_db',
@@ -13,12 +9,26 @@ const scalarDb = getScalar(
   'moviephile123',
   'titles',
   'rating')
-
+  
 const formatPrice = (price) => `£${price}`
+
+const buildPricer = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const pricer = urlParams.get('pricer');  
+  return pricer === 'imdb'
+  ? (id) => price(id, fetchRating)
+  : (id) => price(id, scalarDb)
+}
+
+const priceForMovieWithId = buildPricer()
+
+const buttonSubmit = document.querySelector(".price-calculator__submit")
+const fieldMovieId = document.querySelector(".price-calculator__movie-id")
+const labelPrice = document.querySelector(".price-calculator__price")
 
 buttonSubmit.addEventListener('click', () => {
   const movieId = fieldMovieId.value
-  const moviePrice = price(movieId, scalarDb)
+  const moviePrice = priceForMovieWithId(movieId)
 
   labelPrice.innerHTML = formatPrice(moviePrice)
 })
